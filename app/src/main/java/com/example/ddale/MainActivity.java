94661b0099019fd818099acf9fdf84a67f4198ec
Alban Rahier <ara.rahier@gmail.com>
2019-06-22 13:57:22 +0200

    :sparkles: Add new Activity to choose the route.

--- app/src/main/java/com/example/ddale/MainActivity.java
+++ app/src/main/java/com/example/ddale/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.ddale;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button parcours;
@@ -37,5 +37,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         */
+
+        parcours.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ChoixParcoursActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
