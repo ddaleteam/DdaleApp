2650ebd37fce98844778295a45c208cb7154aa31
ubrisson <50323620+ubrisson@users.noreply.github.com>
2019-06-22 12:04:57 +0200

    :lipstick: Comment out debug button

--- app/src/main/java/com/example/ddale/MainActivity.java
+++ app/src/main/java/com/example/ddale/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.ddale;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button parcours;
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        /*
         Button btnDebugAR = findViewById(R.id.btnDebugAR);
         btnDebugAR.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +36,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(arIntent);
             }
         });
+        */
     }
 }
