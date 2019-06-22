515842daeb346a8f098319edf9c7d8ab6bb3fbce
Inao <thomas.iona20@gmail.com>
2019-06-23 01:52:16 +0200

    :sparkles: Add user location and set initial zoom

--- app/src/main/java/com/example/ddale/MapActivity.java
+++ app/src/main/java/com/example/ddale/MapActivity.java
@@ -1,40 +1,36 @@
 package com.example.ddale;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.ddale.map.DdaleInfoWindow;
 import com.example.ddale.modele.Oeuvre;
-import com.squareup.picasso.Picasso;
 
+import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.Overlay;
-import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
-import org.osmdroid.views.overlay.infowindow.InfoWindow;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MapActivity extends AppCompatActivity {
     MapView map = null;
+    private MyLocationNewOverlay myLocationNewOverlay;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Oeuvre oeuvre = new Oeuvre(1,3.14749 , 50.61014, 0,"Le Radeau de la Méduse","Théodore Géricault", 1818,491,716,"Peinture à l'huile","https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/JEAN_LOUIS_TH%C3%89ODORE_G%C3%89RICAULT_-_La_Balsa_de_la_Medusa_%28Museo_del_Louvre%2C_1818-19%29.jpg/320px-JEAN_LOUIS_TH%C3%89ODORE_G%C3%89RICAULT_-_La_Balsa_de_la_Medusa_%28Museo_del_Louvre%2C_1818-19%29.jpg","");
+        Oeuvre oeuvre = new Oeuvre(1, 3.14749, 50.61014, 0, "Le Radeau de la Méduse", "Théodore Géricault", 1818, 491, 716, "Peinture à l'huile", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/JEAN_LOUIS_TH%C3%89ODORE_G%C3%89RICAULT_-_La_Balsa_de_la_Medusa_%28Museo_del_Louvre%2C_1818-19%29.jpg/320px-JEAN_LOUIS_TH%C3%89ODORE_G%C3%89RICAULT_-_La_Balsa_de_la_Medusa_%28Museo_del_Louvre%2C_1818-19%29.jpg", "");
         super.onCreate(savedInstanceState);
         Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
@@ -51,17 +47,25 @@ public class MapActivity extends AppCompatActivity {
         final List<Overlay> overlays = map.getOverlays();
         ScaleBarOverlay mScaleBarOverlay = new ScaleBarOverlay(map);
         overlays.add(mScaleBarOverlay);
+        myLocationNewOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), map);
+        myLocationNewOverlay.enableMyLocation();
+        map.getOverlays().add(myLocationNewOverlay);
+
 
         Marker startMarker = new Marker(map);
         startMarker.setRelatedObject(oeuvre);
         startMarker.setPosition(oeuvre.getLocalisation());
-        startMarker.setInfoWindow(new DdaleInfoWindow(map,oeuvre));
+        startMarker.setInfoWindow(new DdaleInfoWindow(map, oeuvre));
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         startMarker.setIcon(getDrawable(R.drawable.ic_place_32dp));
         ImageView imageView = new ImageView(this);//Intermédiaire pour le passage de l'image de picasso au marker
         startMarker.setTitle(oeuvre.getTitre());
         startMarker.setSnippet(oeuvre.getAuteur());
         map.getOverlays().add(startMarker);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(20);
+        ((IMapController) mapController).setCenter(new GeoPoint(oeuvre.getLocalisation().getLatitude(),oeuvre.getLocalisation().getLongitude()-0.0001));
     }
 
     public void onResume() {
@@ -81,9 +85,6 @@ public class MapActivity extends AppCompatActivity {
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
-
-
-
 
 
 }
